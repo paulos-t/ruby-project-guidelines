433aeb6808614220c6315f0774643908eb5f7a90
Paulos Thomas <paulost72@gmail.com>
2021-06-24 14:49:08 -0700

    delete

--- Gemfile
+++ Gemfile
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
-gem "sinatra-activerecord"
-gem "sqlite3"
+gem "activerecord"
+# gives additional rake tasks under db namespace
+gem "sinatra-activerecord", "~> 2.0.23"
+gem "rake"
+
+gem "sqlite3", "~> 1.4"
 gem "pry"
 gem "require_all"
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,13 @@
 require 'bundler'
 Bundler.require
 
+# database.yml configures our database connection
+# you might also see it like this in the labs:
+# ActiveRecord::Base.establish_connection(
+#   adapter: 'sqlite3',
+#   database: "db/development.sqlite"
+# )
+
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+# require all files in our lib directory
 require_all 'lib'
